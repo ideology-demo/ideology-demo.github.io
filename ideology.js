d1b86dcc22653a066c25675cd3462bf730d9a741
ideology-demo <89568604+ideology-demo@users.noreply.github.com>
2021-08-26 17:30:30 +0800

    Add files via upload

---
+++ ideology.js
@@ -0,0 +1,38 @@
+ideology = [
+{
+"name": "示例1",
+"stats": {
+        "econ": 50.0,
+        "dipl": 50.0,
+        "govt": 50.0,
+        "scty": 50.0
+}
+},
+{
+"name": "示例2",
+"stats": {
+        "econ": 50.0,
+        "dipl": 50.0,
+        "govt": 50.0,
+        "scty": 50.0
+}
+},
+{
+"name": "示例3",
+"stats": {
+        "econ": 50.0,
+        "dipl": 50.0,
+        "govt": 50.0,
+        "scty": 50.0
+}
+},
+{
+"name": "示例4",
+"stats": {
+        "econ": 50.0,
+        "dipl": 50.0,
+        "govt": 50.0,
+        "scty": 50.0
+}
+},
+];
